3ec783581c610e2702555d354d1b185c8abbcdeb
pawanshaw66 <97239563+pawanshaw66@users.noreply.github.com>
2024-03-04 11:21:46 +0000

    datatypes

---
+++ js-basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") we are using node js not browser
+
+// code readability should be high
+
+let name="pawan"
+let age=23
+console.table([name,age])
+
+// number->2^53
+// bigint
+// string
+// boolean
+// null->standalone value
+// undefined->value not assigned
+// symbol->unique
+// object 
+
+console.log(typeof "pawan")
+console.log(typeof(null))
+console.log(typeof undefined)
