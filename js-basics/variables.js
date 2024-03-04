bac3f82415ce98c02c3221f48c778c23df08bc12
pawanshaw66 <97239563+pawanshaw66@users.noreply.github.com>
2024-03-04 11:01:52 +0000

    variables

---
+++ js-basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 2546878
+let accountEmail = "hello@gmail.com"
+var accountPassword = "25468"
+accountCity = "Kolkata"
+
+let accountState;
+// accountId can't change
+
+console.log(accountId)
+console.log(accountEmail)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var because of issue in block issue and functional issue
+*/
