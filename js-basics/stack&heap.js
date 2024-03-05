94f23c8b73055349e013064948dd0899c62a8349
pawanshaw66 <97239563+pawanshaw66@users.noreply.github.com>
2024-03-05 14:33:14 +0000

    stack and heap

--- js-basics/stack&heap.js
+++
@@ -1,6 +0,0 @@
-// stack -> primitive
-// Heap -> Non-Primitive
-
-let name="pawan"
-let name2=name
-console.log(name2)
---
+++ js-basics/stackheap.js
@@ -0,0 +1,22 @@
+// stack -> primitive
+// Heap -> Non-Primitive
+
+let userName="pawan"
+
+let name2=userName
+
+name2="rohan"
+console.log(name2)
+console.log(userName)
+
+let user ={
+    email:"pawan@gmail.comm",
+    upi:"user@yl"
+}
+
+let user2=user
+
+user2.email="rohan@email.com"
+console.log(user.email)
+console.log(user2.email)
+
